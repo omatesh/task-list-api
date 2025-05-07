6a4f7cbabd4ab92f084fe09088d64e0b879a9cf3
Olya Mateshov <olyamateshov@gmail.com>
2025-05-07 18:12:16 -0400

    Wave 1 and Wave 2

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,37 @@
 from sqlalchemy.orm import Mapped, mapped_column
 from ..db import db
+from datetime import datetime
+from typing import Optional
+from sqlalchemy import DateTime
 
 class Task(db.Model):
+    __tablename__ = "tasks"
+
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str]
+    description: Mapped[str]
+    completed_at: Mapped[Optional[datetime]] = mapped_column(DateTime, nullable=True)
+    #DateTime is a SQLAlchemy column type used to store date and time values in a database
+    #Optional comes from Python's built-in typing module.
+
+    def to_dict(self):
+        task_as_dict = {}
+        task_as_dict["id"] = self.id
+        task_as_dict["title"] = self.title
+        task_as_dict["description"] = self.description
+        # task_as_dict["completed_at"] = self.completed_at
+        task_as_dict["is_complete"] = False
+
+
+        return task_as_dict
+    
+    @classmethod
+    def from_dict(cls, task_data):
+        new_task = Task(title=task_data["title"],
+                        description=task_data["description"],
+                        completed_at=task_data.get("completed_at"))
+        return new_task
+    
+    # @property
+    # def is_complete(self) -> bool:
+    # return self.completed_at is not None
--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -1,1 +1,78 @@
-from flask import Blueprint
+from flask import Blueprint, abort, make_response, request, Response
+from app.models.task import Task
+from ..db import db
+from .route_utilities import validate_model, create_model
+from datetime import datetime, timezone
+
+
+# def get_task_by_id
+
+bp = Blueprint("bp", __name__, url_prefix="/tasks")
+
+@bp.post("")
+def create_task_from_dict():
+    request_body = request.get_json()
+    return create_model(Task, request_body)
+
+# @bp.get("")
+# def get_all_tasks():
+#     return get_models_with_filters(Task, request.args)
+
+@bp.get("/<task_id>")
+def get_one_task_by_id(task_id):
+    task = validate_model(Task, task_id)
+
+    return {"task" : task.to_dict()}
+
+
+@bp.put("/<task_id>")
+def update_task(task_id):
+    task = validate_model(Task, task_id)
+    request_body = request.get_json()
+
+    task.title = request_body["title"]
+    task.description = request_body["description"]
+    task.completed_at = request_body.get("completed_at")
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
+
+@bp.patch("/<task_id>/mark_complete")
+def mark_complete_task(task_id):
+    task = validate_model(Task, task_id)
+ 
+    task.completed_at = datetime.now(timezone.utc)
+
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
+
+@bp.delete("/<task_id>")
+def delete_task(task_id):
+    task = validate_model(Task, task_id)
+    db.session.delete(task)
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
+
+# @bp.get("/tasks")
+@bp.get("")
+def get_all_tasks():
+    query = db.select(Task)
+
+    # Get the sort parameter from the query string
+    sort_param = request.args.get("sort") # retrieves {"sort": "asc"} from URL
+
+    if sort_param:
+        if sort_param.lower() == "asc":
+            query = query.order_by(Task.title.asc())
+        elif sort_param.lower() == "desc":
+            query = query.order_by(Task.title.desc())
+        else:
+            query = query.order_by(Task.id)
+    else:
+        query = query.order_by(Task.id)  # default sort if no sort param given
+
+    tasks = db.session.scalars(query)
+    tasks_response = [task.to_dict() for task in tasks]
+    return tasks_response
