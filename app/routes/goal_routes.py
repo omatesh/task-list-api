12f6a2a768841eb14022088a1af1166be579ef11
omatesh <olyamateshov@gmail.com>
2025-05-24 12:01:37 -0400

    Update app/routes/goal_routes.py
    added lines
    
    Co-authored-by: Mikelle <119449245+mikellewade@users.noreply.github.com>

--- app/routes/goal_routes.py
+++ app/routes/goal_routes.py
@@ -96,6 +96,7 @@ def update_goal(goal_id):
 @bp.delete("/<goal_id>")
 def delete_goal(goal_id):
     goal = validate_model(Goal, goal_id)
+    
     db.session.delete(goal)
     db.session.commit()
 
